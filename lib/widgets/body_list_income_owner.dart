03c527578274703555cd864bf383f71c571513c2
มาสเตอร์ อึ่ง <phrombutr@gmail.com>
2024-10-08 11:20:46 +0700

    Admin Near OK

--- lib/widgets/body_list_income_owner.dart
+++ lib/widgets/body_list_income_owner.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:testdb/models/order_wash_model.dart';
+import 'package:testdb/models/type_cloths_model.dart';
+import 'package:testdb/utility/app_controller.dart';
 import 'package:testdb/utility/app_service.dart';
 
-class BodyListIncomeOwner extends StatelessWidget {
+class BodyListIncomeOwner extends StatefulWidget {
   const BodyListIncomeOwner({super.key});
+
+  @override
+  State<BodyListIncomeOwner> createState() => _BodyListIncomeOwnerState();
+}
+
+class _BodyListIncomeOwnerState extends State<BodyListIncomeOwner> {
+  AppController appController = Get.put(AppController());
+
+  int total = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    processCalTotal();
+  }
+
+  Future<void> processCalTotal() async {
+    var orderWashModels =
+        await AppService().processReadOrderWhereStatus(status: 'Finish');
+
+    for (var element in orderWashModels) {
+      var typeClothModels = await AppService()
+          .readAllTypeClothsFromAmountCloth(amountCloth: element.amountCloth);
+
+      int sum = await AppService()
+          .calculateGrandTotal(typeClothsModels: typeClothModels);
+
+      total = total + sum;
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +50,7 @@ class BodyListIncomeOwner extends StatelessWidget {
           var orderWashModels = snapshot.data!;
 
           if (orderWashModels.isEmpty) {
-            return Text('ยังไม่มี order');
+            return const Text('ยังไม่มี order');
           } else {
             return ListView(
               padding: const EdgeInsets.symmetric(horizontal: 8),
@@ -22,9 +58,10 @@ class BodyListIncomeOwner extends StatelessWidget {
                 displayHead(),
                 displayContent(orderWashModels),
                 const Divider(),
-                Row(mainAxisAlignment: MainAxisAlignment.end,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text('Total : ${AppService().calculateTotal(orderWashModels: orderWashModels)}'),
+                    Text('Total : $total', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
                   ],
                 ),
               ],
@@ -39,75 +76,88 @@ class BodyListIncomeOwner extends StatelessWidget {
 
   ListView displayContent(List<OrderWashModel> orderWashModels) {
     return ListView.builder(
-                physics: const ScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: orderWashModels.length,
-                itemBuilder: (context, index) => Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Text(orderWashModels[index].refWash),
-                    ),
-                    const SizedBox(height: 30,
-                      child: VerticalDivider(),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Text(orderWashModels[index].dateStart, style: const TextStyle(fontSize: 12)),
-                    ),
-                     const SizedBox(height: 30,
-                      child: VerticalDivider(),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Text(orderWashModels[index].dateEnd, style: const TextStyle(fontSize: 12)),
-                    ),
-                     const SizedBox(height: 30,
-                      child: VerticalDivider(),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text(orderWashModels[index].amountCloth),
-                    ),
-                     const SizedBox(height: 30,
-                      child: VerticalDivider(),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text(orderWashModels[index].total),
-                    ),
-                  ],
-                ),
-              );
+      physics: const ScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: orderWashModels.length,
+      itemBuilder: (context, index) => Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Text(orderWashModels[index].refWash),
+          ),
+          const SizedBox(
+            height: 30,
+            child: VerticalDivider(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text(orderWashModels[index].dateStart,
+                style: const TextStyle(fontSize: 12)),
+          ),
+          const SizedBox(
+            height: 30,
+            child: VerticalDivider(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text(orderWashModels[index].dateEnd,
+                style: const TextStyle(fontSize: 12)),
+          ),
+          const SizedBox(
+            height: 30,
+            child: VerticalDivider(),
+          ),
+          Expanded(
+            flex: 1,
+            child: FutureBuilder(
+              future: AppService().readAllTypeClothsFromAmountCloth(
+                  amountCloth: orderWashModels[index].amountCloth),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<TypeClothsModel> typeClothModels = snapshot.data!;
+
+                  // total = total +
+                  //     AppService().calculateGrandTotal(
+                  //         typeClothsModels: typeClothModels);
+
+                  // setState(() {});
+
+                  return Text(
+                      '${AppService().calculateGrandTotal(typeClothsModels: typeClothModels)}');
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Container displayHead() {
     return Container(
-                decoration: BoxDecoration(color: Colors.grey.shade300),
-                child: const Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Text('Ref'),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Text('Start'),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Text('End'),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text('Amt'),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text('Sum'),
-                    ),
-                  ],
-                ),
-              );
+      decoration: BoxDecoration(color: Colors.grey.shade300),
+      child: const Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Text('Ref'),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text('Start'),
+          ),
+          Expanded(
+            flex: 2,
+            child: Text('End'),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text('Sum'),
+          ),
+        ],
+      ),
+    );
   }
 }
